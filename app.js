43d418e01aad97a68c9d6c1644e8573a3362e699
Naveendra Nambula <naveendranambula@gmail.com>
2019-02-20 23:35:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,6 +36,29 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         }
     }    
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    if (gamePlaying) {
+        // Add CURRENT score to GLOBAL score
+        scores[activePlayer] += roundScore;
+
+        // Update the UI
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+        // Check if player won the game
+        if (scores[activePlayer] >= 100) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        } else {
+            //Next player
+            nextPlayer();
+        }
+    }
+});
+
 //add here.
 var scores,roundScore,activePlayer;
 
